hpkfft/nanobind

commit ac63d3a63dfec1be5c1129fa6be3ee35d5ac13ed

Implicit `nb::none()` annotation for `std::optional<T>`-typed arguments (#1262)

include/nanobind/nb_func.h
@@ -95,6 +95,8 @@ NB_INLINE PyObject *func_create(Func &&func, Return (*)(Args...),
         kwargs_pos_n = index_n_v<std::is_same_v<intrinsic_t<Args>, kwargs>...>,
         nargs = sizeof...(Args);
 
+    constexpr bool has_arg_defaults = (detail::has_arg_defaults_v<Args> || ... || false);
+
     // Determine the number of nb::arg/nb::arg_v annotations
     constexpr size_t nargs_provided =
         (std::is_base_of_v<arg, Extra> + ... + 0);
@@ -102,7 +104,7 @@ NB_INLINE PyObject *func_create(Func &&func, Return (*)(Args...),
         (std::is_same_v<is_method, Extra> + ... + 0) != 0;
     constexpr bool is_getter_det =
         (std::is_same_v<is_getter, Extra> + ... + 0) != 0;
-    constexpr bool has_arg_annotations = nargs_provided > 0 && !is_getter_det;
+    constexpr bool has_arg_annotations = has_arg_defaults || (nargs_provided > 0 && !is_getter_det);
 
     // Determine the number of potentially-locked function arguments
     constexpr bool lock_self_det =
@@ -128,7 +130,7 @@ NB_INLINE PyObject *func_create(Func &&func, Return (*)(Args...),
     // A few compile-time consistency checks
     static_assert(args_pos_1 == args_pos_n && kwargs_pos_1 == kwargs_pos_n,
         "Repeated use of nb::kwargs or nb::args in the function signature!");
-    static_assert(!has_arg_annotations || nargs_provided + is_method_det == nargs,
+    static_assert(!has_arg_annotations || has_arg_defaults || nargs_provided + is_method_det == nargs,
         "The number of nb::arg annotations must match the argument count!");
     static_assert(kwargs_pos_1 == nargs || kwargs_pos_1 + 1 == nargs,
         "nb::kwargs must be the last element of the function signature!");
@@ -188,7 +190,15 @@ NB_INLINE PyObject *func_create(Func &&func, Return (*)(Args...),
     };
 
     // The following temporary record will describe the function in detail
-    func_data_prelim<nargs_provided> f;
+    func_data_prelim<has_arg_defaults ? nargs : nargs_provided> f{};
+    if constexpr (has_arg_defaults) {
+        uint8_t def_args[] = {has_arg_defaults_v<Args>...};
+        for (size_t i = 0; i < nargs; ++i) {
+            if (def_args[i]) {
+                f.args[i].flag = cast_flags::accepts_none;
+            }
+        }
+    }
     f.flags = (args_pos_1   < nargs ? (uint32_t) func_flags::has_var_args   : 0) |
               (kwargs_pos_1 < nargs ? (uint32_t) func_flags::has_var_kwargs : 0) |
               (ReturnRef            ? (uint32_t) func_flags::return_ref     : 0) |

include/nanobind/nb_attr.h
@@ -328,7 +328,7 @@ NB_INLINE void func_extra_apply(F &f, const arg &a, size_t &index) {
         flag |= (uint8_t) cast_flags::convert;
 
     arg_data &arg = f.args[index];
-    arg.flag = flag;
+    arg.flag |= flag;
     arg.name = a.name_;
     arg.signature = a.signature_;
     arg.value = nullptr;

include/nanobind/nb_traits.h
@@ -195,6 +195,12 @@ struct is_complex<T, enable_if_t<std::is_same_v<
 template<typename T>
 inline constexpr bool is_complex_v = is_complex<T>::value;
 
+template <typename T>
+struct has_arg_defaults : std::false_type {};
+
+template <typename T>
+constexpr bool has_arg_defaults_v = has_arg_defaults<intrinsic_t<T>>::value;
+
 NAMESPACE_END(detail)
 
 template <typename... Args>

include/nanobind/stl/optional.h
@@ -23,5 +23,8 @@ struct type_caster<std::optional<T>> : optional_caster<std::optional<T>> {};
 
 template <> struct type_caster<std::nullopt_t> : none_caster<std::nullopt_t> { };
 
+template <typename T>
+struct has_arg_defaults<std::optional<T>> : std::true_type {};
+
 NAMESPACE_END(detail)
 NAMESPACE_END(NB_NAMESPACE)

tests/test_stl.cpp
@@ -19,6 +19,8 @@ NB_MAKE_OPAQUE(std::vector<float, std::allocator<float>>)
 
 namespace nb = nanobind;
 
+static_assert(nb::detail::has_arg_defaults_v<std::optional<bool>>);
+
 static int default_constructed = 0, value_constructed = 0, copy_constructed = 0,
            move_constructed = 0, copy_assigned = 0, move_assigned = 0,
            destructed = 0;

tests/test_stl.py
@@ -385,11 +385,7 @@ def test38_std_optional_none():
     t.optional_none(None)
     assert t.optional_cstr(None) == "none"
     assert t.optional_cstr("hi") == "hi"
-
-    # `.none()` to optional argument specifier to avoid costs.
-    # Ref: https://github.com/wjakob/nanobind/pull/950
-    with pytest.raises(TypeError):
-        assert t.optional_non_assignable(None).value == None
+    assert t.optional_non_assignable(None) == None
 
 
 def test39_std_optional_ret_opt_movable(clean):

tests/test_stl_ext.pyi.ref
@@ -167,7 +167,7 @@ def optional_unbound_type(x: int | None = None) -> int | None: ...
 
 def optional_unbound_type_with_nullopt_as_default(x: int | None = None) -> int | None: ...
 
-def optional_non_assignable(arg: NonAssignable, /) -> NonAssignable | None: ...
+def optional_non_assignable(arg: NonAssignable | None) -> NonAssignable | None: ...
 
 def variant_copyable(arg: Copyable | int, /) -> None: ...
 

commit ed7ab31f5ffe313b2ca945573e29112ea5e475b2

Fix regression introduced in PR #1262
PR #1262 added the ability to auto-infer ``nb::none()`` annotations in
function bindings.

This feature was not tested for methods and suffers from an off-by-one
error that keeps it from working in that context: we incorrectly
generate an argument annotation for 'self' too, which shifts the
implicit annotations to the next argument (or even beyond the last
argument, meaning that it will be ignored).

The commit fixes this.

Fixes issue #1281.

include/nanobind/nb_func.h
@@ -190,15 +190,15 @@ NB_INLINE PyObject *func_create(Func &&func, Return (*)(Args...),
     };
 
     // The following temporary record will describe the function in detail
-    func_data_prelim<has_arg_defaults ? nargs : nargs_provided> f;
+    func_data_prelim<has_arg_defaults ? (nargs - is_method_det) : nargs_provided> f;
 
     // Initialize argument flags. The first branch turns std::optional<> types
-    // into implicit nb::none() anotations.
+    // into implicit nb::none() annotations (skipping 'self' for methods).
     if constexpr (has_arg_defaults) {
-        size_t i = 0;
-        ((f.args[i++] = { nullptr, nullptr, nullptr, nullptr,
-            has_arg_defaults_v<Args> ? (uint8_t) cast_flags::accepts_none
-                                     : (uint8_t) 0 }), ...);
+        ((void)(Is < is_method_det ||
+                (f.args[Is - is_method_det] = { nullptr, nullptr, nullptr, nullptr,
+                    has_arg_defaults_v<Args> ? (uint8_t) cast_flags::accepts_none
+                                             : (uint8_t) 0 }, true)), ...);
     } else if constexpr (nargs_provided > 0) {
         for (size_t i = 0; i < nargs_provided; ++i)
             f.args[i].flag = 0;

tests/test_classes.cpp
@@ -60,6 +60,13 @@ struct PairStruct {
     Struct s2;
 };
 
+// Test case for issue #1280
+struct OptionalNoneTest {
+    int compute(int i, std::optional<int> j, int k) const {
+        return i + j.value_or(0) + k;
+    }
+};
+
 struct Big {
     char data[1024];
     Big() { memset(data, 0xff, 1024); }
@@ -186,6 +193,12 @@ NB_MODULE(test_classes_ext, m) {
         .def(nb::init<>())
         .def_rw("s1", &PairStruct::s1, "A documented property")
         .def_rw("s2", &PairStruct::s2);
+
+    // Test case for issue #1280
+    nb::class_<OptionalNoneTest>(m, "OptionalNoneTest")
+        .def(nb::init<>())
+        .def("compute", &OptionalNoneTest::compute,
+             "i"_a, "j"_a = nb::none(), "k"_a = 0);
 
     m.def("stats", []{
         nb::dict d;

tests/test_classes_ext.pyi.ref
@@ -60,6 +60,11 @@ class PairStruct:
     @s2.setter
     def s2(self, arg: Struct, /) -> None: ...
 
+class OptionalNoneTest:
+    def __init__(self) -> None: ...
+
+    def compute(self, i: int, j: int | None = None, k: int = 0) -> int: ...
+
 def stats() -> dict: ...
 
 def reset() -> None: ...